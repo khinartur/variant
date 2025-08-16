0ce24476c74748a813e36565d35acc1db68cb6c5
Arthur Khineltsev <khineltsev@gmail.com>
2025-08-16 10:31:25 +0300

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Variant group test task
+
+## Development
+
+Vite.js is used for project building. For local development run:
+
+```
+yarn dev
+```
---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import {globalIgnores} from 'eslint/config'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config([
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                {allowConstantExport: true},
+            ],
+            'object-curly-spacing': ['error', 'never'],
+            'array-bracket-spacing': ['error', 'never'],
+            curly: ['error', 'all'],
+            'brace-style': ['error', '1tbs', {allowSingleLine: false}],
+        },
+    },
+])
