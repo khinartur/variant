35aefb97b598bea06fd466b6c183497d33f14602
Arthur Khineltsev <khineltsev@gmail.com>
2025-08-20 00:38:52 +0300

    Add react hook form

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,10 @@ export type Letter = {
     id: string
     content: string
 }
+
+export type ApplicationFormData = {
+    jobTitle: string
+    company: string
+    skills: string
+    details: string
+}
--- src/widgets/ui/ApplicationForm/ApplicationForm.tsx
+++ src/widgets/ui/ApplicationForm/ApplicationForm.tsx
@@ -1,9 +1,13 @@
 import clsx from 'clsx'
+import {useCallback} from 'react'
+import {useForm} from 'react-hook-form'
 import {useIsMobile} from '~/shared/hooks'
+import type {ApplicationFormData} from '~/shared/types'
 import {Button, Input, Text, Textarea} from '~/shared/ui'
 import styles from './ApplicationForm.module.css'
 
-interface ApplicationFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface ApplicationFormProps
+    extends React.FormHTMLAttributes<HTMLFormElement> {}
 
 export const ApplicationForm = ({
     className,
@@ -11,8 +15,30 @@ export const ApplicationForm = ({
 }: ApplicationFormProps) => {
     const isMobile = useIsMobile()
 
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isValid},
+    } = useForm<ApplicationFormData>({
+        mode: 'onChange',
+        defaultValues: {
+            jobTitle: '',
+            company: '',
+            skills: '',
+            details: '',
+        },
+    })
+
+    const onSubmit = useCallback((data: ApplicationFormData) => {
+        console.log('Form data:', data)
+    }, [])
+
     return (
-        <div className={clsx(styles.form, className)} {...props}>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className={clsx(styles.form, className)}
+            {...props}
+        >
             <div className={styles.header}>
                 <Text variant="h1" color="secondary">
                     New application
@@ -25,10 +51,21 @@ export const ApplicationForm = ({
                             <Input
                                 label="Job title"
                                 placeholder="Product Manager"
+                                error={!!errors.jobTitle}
+                                {...register('jobTitle', {
+                                    required: true,
+                                })}
                             />
                         </div>
                         <div className={styles.line}>
-                            <Input label="Company" placeholder="Apple" />
+                            <Input
+                                label="Company"
+                                placeholder="Apple"
+                                error={!!errors.company}
+                                {...register('company', {
+                                    required: true,
+                                })}
+                            />
                         </div>
                     </>
                 ) : (
@@ -36,25 +73,53 @@ export const ApplicationForm = ({
                         <Input
                             label="Job title"
                             placeholder="Product Manager"
+                            error={!!errors.jobTitle}
+                            {...register('jobTitle', {
+                                required: true,
+                            })}
                         />
-                        <Input label="Company" placeholder="Apple" />
+                        <Input
+                            label="Company"
+                            placeholder="Apple"
+                            error={!!errors.company}
+                            {...register('company', {
+                                required: true,
+                            })}
+                        />
                     </div>
                 )}
                 <div className={styles.line}>
                     <Input
                         label="I am good at..."
                         placeholder="HTML, CSS and doing things in time"
+                        error={!!errors.skills}
+                        {...register('skills', {
+                            required: true,
+                        })}
                     />
                 </div>
                 <Textarea
                     label="Additional details"
                     placeholder="Describe why you are a great fit or paste your bio"
                     limit={1200}
+                    error={!!errors.details}
+                    {...register('details', {
+                        required: true,
+                        maxLength: {
+                            value: 1200,
+                            message:
+                                'Additional details cannot exceed 1200 characters',
+                        },
+                    })}
                 />
             </div>
-            <Button size={isMobile ? 'md' : 'lg'} disabled>
+            <Button
+                type="submit"
+                size={isMobile ? 'md' : 'lg'}
+                disabled={!isValid}
+            >
                 Generate Now
             </Button>
-        </div>
+        </form>
     )
 }
